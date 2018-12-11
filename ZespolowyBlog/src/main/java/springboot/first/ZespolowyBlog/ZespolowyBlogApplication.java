2f2bbed3874167377f2e9b8398fbe5595c69d23b
Rgosik <grafal96@gmail.com>
2018-12-11 16:26:36 +0100

    added template

---
+++ ZespolowyBlog/src/main/java/springboot/first/ZespolowyBlog/ZespolowyBlogApplication.java
@@ -0,0 +1,12 @@
+package springboot.first.ZespolowyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZespolowyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZespolowyBlogApplication.class, args);
+	}
+}
