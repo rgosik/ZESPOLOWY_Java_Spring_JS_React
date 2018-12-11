d49f8e4f2e2d84e53f9bec6acfeb9baef4feaa2a
Rgosik <grafal96@gmail.com>
2018-12-11 17:30:46 +0100

    adding Spring HATEOAS

--- ZespolowyBlog/src/main/java/springboot/first/ZespolowyBlog/controllers/BlogController.java
+++ ZespolowyBlog/src/main/java/springboot/first/ZespolowyBlog/controllers/BlogController.java
@@ -1,7 +1,10 @@
 package springboot.first.ZespolowyBlog.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import springboot.first.ZespolowyBlog.models.Blog;
+import springboot.first.ZespolowyBlog.models.BlogNotFoundException;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import springboot.first.ZespolowyBlog.models.BlogNotFoundException;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 
 @RestController
 public class BlogController {
@@ -20,11 +27,20 @@ public class BlogController {
         this.repository = repository;
     }
 
-    @GetMapping("/blogs")
-    List<Blog> all() {
-        return repository.findAll();
-    }
+    // aggregate root //
 
+    @GetMapping("/blogs")
+    Resources<Resource<Blog>> all() {
+
+        List<Resource<Blog>> blogs = repository.findAll().stream()
+                .map(Blog -> new Resource<>(Blog,
+                        linkTo(methodOn(BlogController.class).one(Blog.getId())).withSelfRel(),
+                        linkTo(methodOn(BlogController.class).all()).withRel("blogs")))
+                .collect(Collectors.toList());
+
+        return new Resources<>(blogs, linkTo(methodOn(BlogController.class).all()).withSelfRel());
+    }
+    
     @PostMapping("/blogs")
     Blog newBlog(@RequestBody Blog newBlog) {
         return repository.save(newBlog);
@@ -33,10 +49,14 @@ public class BlogController {
     // Single item //
 
     @GetMapping("/blogs/{id}")
-    Blog one(@PathVariable Long id) {
+    Resource<Blog> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Blog blog = repository.findById(id)
                 .orElseThrow(() -> new BlogNotFoundException(id));
+
+        return new Resource<>(blog,
+                linkTo(methodOn(BlogController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(BlogController.class).all()).withRel("blogs"));
     }
 
     @PutMapping("/blogs/{id}")
