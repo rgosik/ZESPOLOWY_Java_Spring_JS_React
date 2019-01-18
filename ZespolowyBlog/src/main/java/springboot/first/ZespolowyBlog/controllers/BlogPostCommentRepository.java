a9307fe184d186f5a1d897579f1d6feb27d7a9a0
Rgosik <grafal96@gmail.com>
2019-01-18 12:27:11 +0100

    added BlogPostCommentController

--- ZespolowyBlog/src/main/java/springboot/first/ZespolowyBlog/controllers/BlogPostCommentRepository.java
+++ ZespolowyBlog/src/main/java/springboot/first/ZespolowyBlog/controllers/BlogPostCommentRepository.java
@@ -3,6 +3,6 @@ package springboot.first.ZespolowyBlog.controllers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import springboot.first.ZespolowyBlog.models.BlogPostComment;
 
-interface BlogPosCommentRepository extends JpaRepository<BlogPostComment, Long> {
+interface BlogPostCommentRepository extends JpaRepository<BlogPostComment, Long> {
 
 }
