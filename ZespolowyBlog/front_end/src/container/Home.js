cd14ed9d8dec0407f5b7fc148b4660bd1640b82a
Bezsilny <julek200@gmail.com>
2019-01-19 18:02:05 +0100

    Home.js css file

--- ZespolowyBlog/front_end/src/container/Home.js
+++ ZespolowyBlog/front_end/src/container/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import '../css/Home.css';
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="wrapper">
                 <h2>Strona startowa</h2>
             </div>
         );
