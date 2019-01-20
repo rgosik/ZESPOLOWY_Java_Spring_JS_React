cf49469a7a1203560ba395288a28f232789fa80d
wbilikiewicz <wdziurdzia@sigma.ug.edu.pl>
2019-01-20 14:27:22 +0100

    Update Home.js

--- ZespolowyBlog/front_end/src/container/Home.js
+++ ZespolowyBlog/front_end/src/container/Home.js
@@ -4,8 +4,22 @@ import '../css/Home.css';
 class Home extends Component {
     render() {
         return (
-            <div className="wrapper">
-                <h2>Strona startowa</h2>
+            <div>
+                <h2 className="title"></h2>
+                  <div className="col-lg-10 col-md-10 col-xs-12 offset-lg-1 offset-md-1">
+                    <div id="discover">
+                      <h1>Discover the passions of other people !</h1>
+                    </div>
+                    <div id="share">
+                      <h1>Share your experiences !</h1>
+                    </div>
+                    <div id="meet">
+                      <h1>Meet new people !</h1>
+                    </div>
+                    <div id="comment">
+                      <h1>Express your opinions !</h1>
+                    </div>
+                  </div>
             </div>
         );
     }
